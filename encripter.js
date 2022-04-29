1e99eb40c2c0b3f004c294f981a47cabd60b38e7
freitas2021 <assis.freitas@ymail.com>
2022-04-29 14:57:54 -0300

    Apenas um erro

--- encripter.js
+++ encripter.js
@@ -1,19 +1,35 @@
-//Pega o texto e joga no campo resultado
 function encriptar() {
-  document.getElementById("campo2").value =
-    document.getElementById("campo1").value;
+  if (campo1.value.length == 0) {
+    alert("digite algum texto");
+  } else {
+    result.value = campo1.value
+      .replace(/[e|é|è|ê|ể|ë]/g, "enter")
+      .replace(/[i|í|ì|î]/g, "imea")
+      .replace(/[a|à|á|â|ã|ä|å]/g, "ai")
+      .replace(/[o|ó|ò|ô|ö]/g, "ober")
+      .replace(/[u|ú|ù|ü]/g, "ufat")
+      .replace(/[$|@|!|*|%||¨|{|}|#]/, " ");
 
-  document.getElementById("campo1").value = "";
+    document.getElementById("campo1").value = "";
+  }
 }
 
-function desencriptar(){
-    
+function desencriptar() {
+  result.value = campo1.value 
+    .replace(/enter/g, "e")
+    .replace(/imea/g, "i")
+    .replace(/ai/g, "a")
+    .replace(/ober/g, "o")
+    .replace(/ufat/g, "u")
+    .replace(/[$|@|!|*|%||¨|{|}|#]/, " ");
+
+  result.value = "";
 }
+
 function copyToClipBoard() {
-  var content = document.getElementById("campo2");
+  var content = document.getElementById("result");
 
   content.select();
   document.execCommand("copy");
-  document.getElementById("campo2").value = "";
-  
+  document.getElementById("result").value = "";
 }
